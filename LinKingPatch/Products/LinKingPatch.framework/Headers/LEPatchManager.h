ff86f70211d5fda3ddce3aadd07528704b022246
MrDML <dml1630@163.com>
2020-10-26 12:10:05 +0800

    update

--- LinKingPatch/Products/LinKingPatch.framework/Headers/LEPatchManager.h
+++ LinKingPatch/Products/LinKingPatch.framework/Headers/LEPatchManager.h
@@ -14,13 +14,13 @@ NS_ASSUME_NONNULL_BEGIN
  * 当前网络状态
  */
 @property (nonatomic , assign)NetworkStatus        networkStatus;
-@property (nonatomic,copy) void(^downloadProgress)(float progress);
 @property (nonatomic,copy) void(^reachableHandler)(BOOL success);
 
-
 + (instancetype)shared;
+
 /// 将项目中的资源文件压缩移动到沙盒
-/// @param unZipComplete 完成回调
+/// @param projectRootName 本地资源根目录名
+/// @param unZipComplete 回调
 - (void)compressionAndMoveDirWithProjectRootName:(NSString *)projectRootName unZipComplete:(void(^)(void))unZipComplete;
 /// 下载服务端补丁文件
 /// @param sourceURL 远程地址
@@ -32,6 +32,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)startUpdateResources:(void(^)(float progress, NSDictionary *fileInfo))progress downloadComplete:(void(^)(NSError *error,NSArray *totoalUpdateFileInfos))complete;
 /// 递归目录生成目下所有文件信息到补丁文件(content.json)
 /// @param serverURL 远程服务根路径
+/// @param serverProjectRootPath 项目路径
+/// @param localProjectRootName 本地项目更目录名
 /// @param version 版本号 例如 0.0.1 每次热更新需要版本号新增1
 /// @param savePath 生成conten.json 保存的路径
 - (void)generateContentJsonWithServer:(NSString *)serverURL serverProjectRootPath:(NSString *)serverProjectRootPath localProjectRootName:(NSString *)localProjectRootName version:(NSString *)version saveLocalPath:(NSString *)savePath;
@@ -40,7 +42,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)compressionAndMoveDir:(void(^)(void))unZipComplete;
 /// 获取缓存目录的根目录
 - (NSString *)getCacheRootFilePath;
-
+/// 清除缓存
 - (void)clearCacheWebDir;
 
 @end
